5ac98135df56eae8b880f647cbdff991f18ef0f0
fabricionetto <agostinhonetto73@gmail.com>
2025-03-11 16:36:24 -0300

    Crud

--- src/main/java/com/estudo/JDBCTemplate/Controle/Controlador.java
+++ src/main/java/com/estudo/JDBCTemplate/Controle/Controlador.java
@@ -1,13 +1,23 @@
 package com.estudo.JDBCTemplate.Controle;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.estudo.JDBCTemplate.modelo.Camisa;
 import com.estudo.JDBCTemplate.repositorio.Repositorio;
+
+import jakarta.validation.Valid;
 
 @RestController 
 @RequestMapping("/api/camisas")
@@ -23,5 +33,25 @@ public class Controlador {
 	@GetMapping
 	public List<Camisa> ListaCamisas() {
 		return repositorio.buscarCamisas();	
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Camisa> buscarListaId(@PathVariable Integer id){
+		return repositorio.buscarListaId(id);
+	}
+	
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping 
+	public void inserir(@Valid @RequestBody Camisa camisa) {
+		repositorio.salvarProduto(camisa);
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizar(@RequestBody Camisa camisa, @PathVariable Integer id ) {
+		if(!repositorio.existeId(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Conteudo não encotrado");
+		}
+		repositorio.salvarProduto(camisa);
 	}
 }
--- src/main/java/com/estudo/JDBCTemplate/repositorio/Repositorio.java
+++ src/main/java/com/estudo/JDBCTemplate/repositorio/Repositorio.java
@@ -2,12 +2,14 @@ package com.estudo.JDBCTemplate.repositorio;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import com.estudo.JDBCTemplate.modelo.Camisa;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
 
 @Repository
 public class Repositorio {
@@ -23,6 +25,22 @@ public class Repositorio {
 	
 	public List<Camisa> buscarCamisas(){
 		return listaCamisa;
+	}
+
+
+	public Optional<Camisa> buscarListaId(Integer id) {
+		return listaCamisa.stream().filter( obj -> obj.id().equals(id)).findFirst();
+	}
+
+
+	public void salvarProduto(@Valid Camisa camisa) {
+		
+		listaCamisa.add(camisa);
+		
+	}
+
+	public boolean existeId(Integer id) {
+		return listaCamisa.stream().filter(obj -> obj.id().equals(id)).count() == 1;
 	}
 	
 }
